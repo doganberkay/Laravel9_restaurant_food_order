e700ab7c4d578ec8c92c00745d069b1aa278b42b
Berkay Doğan <berkaydogan.mail@gmail.com>
2022-03-17 15:46:35 +0300

    changed the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home.index');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::redirect('/anasayfa', '/home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('Home');
+Route::get('/about', [HomeController::class, 'about'])->name('About');
+Route::get('/contact', [HomeController::class, 'contact'])->name('Contact');
+Route::get('/shop', [HomeController::class, 'shop'])->name('Shop');
+
+//admin
+Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('Admin Panel');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
